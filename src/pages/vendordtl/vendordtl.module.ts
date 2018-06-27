beb7d08524fe13507c15627576b2e9143d322731
Francois Botha <botha.francois@gmail.com>
2018-06-27 19:01:14 +0200

    Add vendordtl page

---
+++ src/pages/vendordtl/vendordtl.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { VendordtlPage } from './vendordtl';
+
+@NgModule({
+  declarations: [
+    VendordtlPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(VendordtlPage),
+  ],
+})
+export class VendordtlPageModule {}
